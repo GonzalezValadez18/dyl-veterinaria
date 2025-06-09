97f2af602798956cd2a1d581201e02e2ec585abb
Jose Leonardo Gonzalez Valadez <jleonardo18071999@gmail.com>
2025-06-09 16:09:55 -0600

    corregir ruta

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    base: "/dyl'veterinaria/", 
+    base: "/dyl-veterinaria/", 
     plugins: [react()],
 });
